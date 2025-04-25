6a783d55fd3c4238ac795da9ad1e78f10437a39d
wither7xx <142383974+wither7xx@users.noreply.github.com>
2025-04-25 18:16:04 +0800

    添加个人信息功能

--- Pages/person_Index.cshtml.cs
+++ Pages/person_Index.cshtml.cs
@@ -1,12 +1,48 @@
+using LostAndFoundWebApp.Models;
+using LostAndFoundWebApp.Services.Mysql;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+using System.Xml.Linq;
 
 namespace LostAndFoundWebApp.Pages
 {
     public class PersonModel : PageModel
     {
+        [BindProperty]
+        public int UserId { get; set; } = 0;
+
+        [BindProperty]
+        public string Name { get; set; } = string.Empty;
+
+        [BindProperty]
+        public string Email { get; set; } = string.Empty;
+
+        [BindProperty]
+        public List<Models.Claim> Claims { get; set; } = [];
         public void OnGet()
         {
+            int userID;
+            try
+            {
+                var userIDClaim = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                userID = int.Parse(userIDClaim ?? string.Empty);
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("userID", "用户未登录");
+                return;
+            }
+            var user = DatabaseOperate.GetUserById(userID);
+            if (user == null)
+            {
+                ModelState.AddModelError("user", "用户不存在");
+                return;
+            }
+            UserId = user.UserId;
+            Name = user.Name;
+            Email = user.Email;
+            Claims = DatabaseOperate.GetClaimsUser(userID);
         }
     }
 }
